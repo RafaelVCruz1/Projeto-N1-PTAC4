6a2c5814ed0cd952d32bc241d61c2ac4f12eb3d3
RafaelVCruz1 <rafael.cruz3@estudante.ifms.edu.br>
2023-08-15 16:39:10 -0400

    Lista de usuarios criada

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,38 @@
 'use server'
 
+const user = [
+    {
+      name: 'Rafael',
+      email: 'rafa@marc.com',
+      password: '123',
+      token: ''
+    },
+    {
+      name: 'Renato',
+      email: 'renas@marc.com',
+      password: '123',
+      token: ''
+    },
+    {
+        name: 'Arthur',
+        email: 'arthur@marc.com',
+        password: '123',
+        token: ''
+      },
+      {
+        name: 'Marcelino',
+        email: 'marcelino@marc.com',
+        password: '123',
+        token: ''
+      },
+  ];
+
 const getUserAuthenticated = (user) => {
 
 }
 
 const getUsers = () =>{
-        
+    
+      
 }
 export { getUsers, getUserAuthenticated };
