94199b2f9d2821b73416959b789795ec7c4f4be7
Rafael Venancio Cruz <venanciocruzrafael@gmail.com>
2023-08-28 23:41:01 -0300

    Final

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,38 +1,44 @@
-'use server'
+"use server";
 
-const user = [
-    {
-      name: 'Rafael',
-      email: 'rafa@marc.com',
-      password: '123',
-      token: ''
-    },
-    {
-      name: 'Renato',
-      email: 'renas@marc.com',
-      password: '123',
-      token: ''
-    },
-    {
-        name: 'Arthur',
-        email: 'arthur@marc.com',
-        password: '123',
-        token: ''
-      },
-      {
-        name: 'Marcelino',
-        email: 'marcelino@marc.com',
-        password: '123',
-        token: ''
-      },
-  ];
+const usuarios = [
+  {
+    name: 'Rafael',
+    email: 'rafa@marc.com',
+    password: '123',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+  },
+  {
+    name: 'Renato',
+    email: 'renas@marc.com',
+    password: '123',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+  },
+  {
+    name: 'Arthur',
+    email: 'arthur@marc.com',
+    password: '123',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+  },
+  {
+    name: 'Marcelino',
+    email: 'marcelino@marc.com',
+    password: '123',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+  }
+];
 
 const getUserAuthenticated = (user) => {
+    let userAuth = {}
+    usuarios.map(userzinho => {
+      if (userzinho.email === user.email && userzinho.senha === user.password) {
+        userAuth = userzinho
+      } 
+    })
+    return userAuth
+};
 
-}
+const getUsers = () => {
+    return usuarios
+};
 
-const getUsers = () =>{
-    
-      
-}
 export { getUsers, getUserAuthenticated };
--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,11 +1,18 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
+import ListUser from '@/app/ListUser/ListUser';
+import { Suspense } from "react";
 
 export default async function Dashboard() {
-   
+    let lista = getUsers();
+    let usuario = getUsers();
     return (
         <div>
-            <h1>Dashboard</h1>
-            
+            <Suspense fallback={<p>carregando...</p>}>
+                <ListUser users={usuario}/>
+                {lista.map((user) => (
+                    <h1>{user.name}</h1>
+                ))}
+            </Suspense>
         </div>
     );
 };
